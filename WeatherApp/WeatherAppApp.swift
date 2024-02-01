c8e8e1f0d91a95b535f94ad8dd8d15029877afa4
Maksym Chernenko <maksym.chernenko@gmail.com>
2024-01-31 22:44:01 -0800

    initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Maksym Chernenko on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
